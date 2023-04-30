fe843052661393beafc681572d55c510bf63eab0
ChrGri <christophergrimm88@web.de>
2023-04-30 08:29:36 +0200

    Correct current sensor position, more robust loadcell offset compensation
    - position values from 16bit to 32bit, as I had overflow before
    - precompute pedal stiffness in setup to save runtime
    - obtain current position from stepper. Before it was assumed that stepper finished movement within one cycle, resulting in wrong position
    - modified loadcell offset compensation to subtract the estimated offset. Before, sometimes the initial position was lost. Now, it seems to be more stable

--- Arduino/Prototye_V1.ino
+++ Arduino/Prototye_V1.ino
@@ -44,23 +44,32 @@ float sensor1;
 movingAvgFloat Force_Current_MA(40);      //Sets the number of data points to use when calculating the moving average 
 
 float conversion = 4000; //conversion factor
-float offset = 0;     //offset value
+float loadcellOffset = 0;     //offset value
 
 //Variable for calculation pedal movement & Stepper Parameters
-  int Force_Min = 0.25;    //Min Force in lb to activate Movement
-  int Force_Max = 50;     //Max Force in lb = Max Travel Position
+  float Force_Min = 0.1;    //Min Force in lb to activate Movement
+  float Force_Max = 10.;     //Max Force in lb = Max Travel Position
   float Force_Current = 0;
   
-  int Position_Min = 0;       //Pedal starting Postion. In the future this could be found using a switch
-  int Position_Max = 90;     //Pedal maxium distance to travel
-  int Position_Current = 0;   //Current position of pedal
-  int Position_Next = 0;      //Future pedal postion based on current load cell data
-  int Position_Deadzone = 5;  //Number of steps required before the pedal will move. Added in to prevent oscillation caused by varaying load cell readings
+  long  Position_Min = 0;       //Pedal starting Postion. In the future this could be found using a switch
+  long  Position_Max = 90;     //Pedal maxium distance to travel
+  long  Position_Current = 0;   //Current position of pedal
+  long  Position_Next = 0;      //Future pedal postion based on current load cell data
+  long  Position_Deadzone = 5;  //Number of steps required before the pedal will move. Added in to prevent oscillation caused by varaying load cell readings
   
   float steps_per_rev = 1600;
   float rpm = 2500;
   float speed = (rpm/60*steps_per_rev);   //needs to be in us per step || 1 sec = 1000000 us
   float accel = 1000000;
+
+  long previousTime = 0;
+
+
+  float springStiffnesss = 1;
+  float springStiffnesssInv = 1;
+
+  long prevPosition = 0;
+  long currentPosition = 0;
 
 void setup()
 {
@@ -110,27 +119,22 @@ void setup()
     adc.setChannel(0,1);   // Set the MUX for differential between ch2 and 3 
     sensor1 = (adc.readCurrentChannel()*conversion); // DOUT arriving here are from MUX AIN0 and AIN1
 
- //Load Cell Offset
-  int i;
-  float sval = 0;
+  // Due to construction and gravity, the loadcell measures an initial voltage difference.
+  // To compensate this difference, the difference is estimated by moving average filter.
   float ival = 0;
-  int samples = 1000;
+  long samples = 1000;
   
-  for (i = 0; i < samples; i++){
+  for (long i = 0; i < samples; i++){
     adc.setChannel(0,1);   // Set the MUX for differential between ch2 and 3 
-    sensor1 = (adc.readCurrentChannel()*conversion); // DOUT arriving here are from MUX AIN0 and 
-    ival = sensor1;
+    sensor1 = adc.readCurrentChannel(); // DOUT arriving here are from MUX AIN0 and 
+    ival = sensor1 / samples;
     Serial.println(sensor1,10);
-    sval = sval + ival;
-    
+    loadcellOffset += ival;
   }
 
-  offset = sval/samples;
-  conversion = -0.30/offset*conversion;  //ADS1256 doesn't always have a consitent conversion factor so I had to add this in 
-
-    Serial.print("Offset ");
-    Serial.println(offset,10);
-    delay(1000);
+  Serial.print("Offset ");
+  Serial.println(loadcellOffset,10);
+  delay(1000);
 
     
   //FastAccelStepper setup
@@ -147,59 +151,58 @@ void setup()
 
   }
 
-// Setting Min and Max Travel Position
- 
-limitSwitchMin.setDebounceTime(50); // set debounce time to 50 milliseconds
-limitSwitchMax.setDebounceTime(50); // set debounce time to 50 milliseconds
+  // Setting Min and Max Travel Position
+  
+  limitSwitchMin.setDebounceTime(50); // set debounce time to 50 milliseconds
+  limitSwitchMax.setDebounceTime(50); // set debounce time to 50 milliseconds
 
  limitSwitchMin.loop();  // MUST call the loop() function first
  limitSwitchMax.loop();  // MUST call the loop() function first
 
-  //int stateMin = limitSwitchMin.getState();
-  //int stateMax = limitSwitchMax.getState();
+  //long stateMin = limitSwitchMin.getState();
+  //long stateMax = limitSwitchMax.getState();
   
 
-     int set = 0;                                                 //Setting Max Limit Switch Position
-      while(limitSwitchMax.getState() == HIGH){
-      stepper->setSpeedInHz(speed);   // steps/s      
-      stepper->moveTo(set);       //Move motor 10 steps back
-        
-        while (stepper->getCurrentPosition() != stepper->targetPos()){}
-        
+  long set = 0;                                                 //Setting Max Limit Switch Position
+  while(limitSwitchMax.getState() == HIGH){
+  stepper->setSpeedInHz(speed);   // steps/s      
+  stepper->moveTo(set);       //Move motor 10 steps back
+    
+    while (stepper->getCurrentPosition() != stepper->targetPos()){}
+    
 
-      set = set + 100;
-      Serial.println(set);
-          
+    set = set + 100;
+    Serial.println(set);
+        
     limitSwitchMax.loop();  // MUST call the loop() function first
 
  
-      if(limitSwitchMax.getState() == LOW){
-          Serial.println("The limit switch: Max On");
-          Serial.print("Max Position is "); 
-          Serial.println(stepper->getCurrentPosition()-1000);
+    if(limitSwitchMax.getState() == LOW){
+        Serial.println("The limit switch: Max On");
+        Serial.print("Max Position is "); 
+        Serial.println(stepper->getCurrentPosition()-1000);
 
-        Position_Max = (stepper->getCurrentPosition()-1000);
-    }
+      Position_Max = (stepper->getCurrentPosition()-1000);
+    } 
   }    
 
-      while(limitSwitchMin.getState() == HIGH){                    //Setting Min Limit Switch Position
-        stepper->setSpeedInHz(speed);   // steps/s
-        stepper->moveTo(set);       //Move motor 10 steps back
+  while(limitSwitchMin.getState() == HIGH){                    //Setting Min Limit Switch Position
+    stepper->setSpeedInHz(speed);   // steps/s
+    stepper->moveTo(set);       //Move motor 10 steps back
 
-          while (stepper->getCurrentPosition() != stepper->targetPos()){}
+    while (stepper->getCurrentPosition() != stepper->targetPos()){}
 
-      set = set - 100;
-      Serial.println(set);
+    set = set - 100;
+    Serial.println(set);
 
-  limitSwitchMin.loop();  // MUST call the loop() function first
+    limitSwitchMin.loop();  // MUST call the loop() function first
 
     if(limitSwitchMin.getState() == LOW){
-    Serial.println("The limit switch: Min On");
-    Serial.print("Min Position is ");
-    Serial.println(stepper->getCurrentPosition()+6000);
-    
-    Position_Min = (stepper->getCurrentPosition()+6000);
-     
+      Serial.println("The limit switch: Min On");
+      Serial.print("Min Position is ");
+      Serial.println(stepper->getCurrentPosition()+6000);
+      
+      Position_Min = (stepper->getCurrentPosition()+6000);
     }
   }   
 
@@ -210,40 +213,82 @@ stepper->moveTo(Position_Min);
 
 stepper->setSpeedInHz(speed);
 
+  // compute pedal stiffness parameters
+  springStiffnesss = (Force_Max-Force_Min) / (float)(Position_Max-Position_Min);
+  springStiffnesssInv = 1.0 / springStiffnesss;
+
+  // obtain current stepper position
+  prevPosition = stepper->getCurrentPosition();
+
 }
 
 void loop()
 { 
 
-adc.waitDRDY(); // wait for DRDY to go low before changing multiplexer register
+
+  // obtain time
+  long currentTime = millis();
+  long elapsedTime = currentTime - previousTime;
+  previousTime = currentTime;
+
+  // obtain loadcell reading measurement
+  adc.waitDRDY(); // wait for DRDY to go low before changing multiplexer register
   adc.setChannel(0,1);   // Set the MUX for differential between ch2 and 3 
-  sensor1 = adc.readCurrentChannel()*conversion; // DOUT arriving here are from MUX AIN0 and AIN1
+  sensor1 = adc.readCurrentChannel(); // DOUT arriving here are from MUX AIN0 and AIN1
+
+  // convert loadcell reading to force
+  sensor1 = ( sensor1 - loadcellOffset ) * conversion;
+  sensor1 = -1.0f * sensor1; // flip sign to make pressure positive
+
+  // low pass filter the force readings
   Force_Current = Force_Current_MA.reading(sensor1);
 
- 
-  if (Force_Current > Force_Max)  {       //If current force is over the max force it will just read the max force
-    Force_Current = Force_Max;
+  // get current stepper position
+  currentPosition = stepper->getCurrentPosition();
+
+  // estimate pedal force change in next cycle
+//#define COMPENSATE_FORCE_CHANGE
+#ifdef COMPENSATE_FORCE_CHANGE
+  float expectedForceIncrease = 1.0 * springStiffnesss * (float)(currentPosition - prevPosition);
+  Force_Current -= expectedForceIncrease;
+#endif
+  // estimate target pedal position
+  Position_Next = springStiffnesssInv * (Force_Current-Force_Min) + Position_Min ;        //Calculates new position using linear function
+  
+  // clip target pedal position
+  if (Position_Next > Position_Max)  {       //If current force is over the max force it will just read the max force
+    Position_Next = Position_Max;
+  }
+  if (Position_Next < Position_Min)  {       //If current force is below the min force it will just read 0
+    Position_Next = Position_Min;
   }
 
-  if (Force_Current < Force_Min)  {       //If current force is below the min force it will just read 0
-    Force_Current = Force_Min;
-  }
-  
-  Position_Next = ((Position_Max-Position_Min)/(Force_Max-Force_Min))*(Force_Current-Force_Min)+Position_Min ;        //Calculates new position using linear function
-  
-  Serial.print(Force_Current);
-  Serial.print(" lbs || ");
+// write debug info to serial monitor
+#define DEBUG_OUTPUT
+#ifdef DEBUG_OUTPUT
+  Serial.print("elapsedTime:");
+  Serial.print(elapsedTime);
+  Serial.print(",");
+  Serial.print("instantaneousForceMeasured:");
+  Serial.print(sensor1,6);
+  Serial.print(",");
+  Serial.print("filteredForceMeasured:");
+  Serial.print(Force_Current, 6);
+  Serial.print(",");
+  Serial.print("TravelDistance:");
   Serial.print(Position_Next - Position_Current);
-  Serial.print(" Travel Distance ");
+  Serial.print(",");
+  Serial.print("TargetPosition:");
   Serial.print(Position_Next);
-  Serial.print(" senosr lbs ");
-  Serial.println(sensor1,4);
+  Serial.println(" ");
+#endif
 
   if(abs(Position_Next-Position_Current)>Position_Deadzone){       //Checks to see if the new position move is greater than the deadzone limit set to prevent oscillation
-  
-  stepper->moveTo(Position_Next);
-
-  Position_Current = Position_Next;      //Sets the new current position after the stepper motors moves !!!This has to be after the stepper have moved
+    stepper->moveTo(Position_Next);
   }
+
+
+  // 
+  prevPosition = currentPosition;
 
  }
